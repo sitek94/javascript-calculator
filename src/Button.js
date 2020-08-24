650f97c0e61c955ece433d11218ca0d361d508b9
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-24 13:58:20 +0200

    Add key down event listeners for buttons

--- src/Button.js
+++ src/Button.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Button.module.scss';
 
 
-function ButtonBase({ className, ...props}) {
+function ButtonBase({ className, keyCodes, value, onClick, ...props}) {
+
+  const handleClick = (e) => {
+    onClick(value)
+  }
+
+  const handleKeyDown = (e) => {
+    if (keyCodes.includes(e.keyCode)) {
+      console.log(e);
+      onClick(value);
+      console.log(value);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+  })
+
+
+
   return (
     <input
       type="button"
+      value={value}
       className={[classes.Base, className].join(' ')}
+      onClick={handleClick}
       {...props}
     />
   );
+}
+
+ButtonBase.defaultProps = {
+  keyCodes: []
 }
 
 // I'm taking id out and assign it to each button as class name
